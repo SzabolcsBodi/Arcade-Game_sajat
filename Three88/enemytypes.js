b7c864c84342fc2eb995ac3924d30a3eb92c2cca
Ballam906 <90650752+Ballam906@users.noreply.github.com>
2025-05-22 14:32:22 +0200

    Add files via upload

---
+++ Three88/enemytypes.js
@@ -0,0 +1,20 @@
+export const enemyTypes = {
+    enemy1: {
+        walk: ['ellenseg/demon/walk1.png', 'ellenseg/demon/walk2.png', 'ellenseg/demon/walk3.png', 'ellenseg/demon/walk4.png'],
+        attack: ['ellenseg/demon/attack1.png', 'ellenseg/demon/attack2.png'],
+        speed: 0.01,
+        size: { x: 0.8, y: 1.1 }
+    },
+    enemy2: {
+        walk: ['ellenseg/soldier/walk1.png', 'ellenseg/soldier/walk2.png', 'ellenseg/soldier/walk3.png', 'ellenseg/soldier/walk4.png'],
+        attack: ['ellenseg/soldier/attack1.png', 'ellenseg/soldier/attack2.png'],
+        speed: 0.008,
+        size: { x: 0.3, y: 0.6 }
+    },
+    enemy3: {
+        walk: ['ellenseg/caco/walk1.png', 'ellenseg/caco/walk2.png', 'ellenseg/caco/walk3.png'],
+        attack: ['ellenseg/caco/attack1.png', 'ellenseg/caco/attack2.png', 'ellenseg/caco/attack3.png', 'ellenseg/caco/attack4.png', 'ellenseg/caco/attack5.png'],
+        speed: 0.012,
+        size: { x: 0.7, y: 0.7 }
+    }
+};
